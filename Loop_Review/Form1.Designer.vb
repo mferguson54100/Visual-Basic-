519625e65966d04b456eab73e38cdc29f81b9836
michael Ferguson <mferguson54100@gmail.com>
2018-05-14 17:39:34 -0700

    initial commit

---
+++ Loop_Review/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstNumber = New System.Windows.Forms.ListBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.ctnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstNumber
+        '
+        Me.lstNumber.FormattingEnabled = True
+        Me.lstNumber.Location = New System.Drawing.Point(41, 12)
+        Me.lstNumber.Name = "lstNumber"
+        Me.lstNumber.Size = New System.Drawing.Size(195, 134)
+        Me.lstNumber.TabIndex = 0
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(41, 166)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(161, 166)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 2
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'ctnClear
+        '
+        Me.ctnClear.Location = New System.Drawing.Point(41, 209)
+        Me.ctnClear.Name = "ctnClear"
+        Me.ctnClear.Size = New System.Drawing.Size(75, 23)
+        Me.ctnClear.TabIndex = 3
+        Me.ctnClear.Text = "Clear"
+        Me.ctnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(161, 209)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.ctnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lstNumber)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstNumber As ListBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents ctnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Loop_Review/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Dim intNumber As Integer
+        Dim strNumber As String
+        Dim intCount As Integer
+
+
+        If Integer.TryParse(intNumber, strNumber) Then
+            If intNumber >= 0 Then
+                intCount = 1
+                For intCount = 1 To 5 'test the condition
+                    strNumber = InputBox("Please enter number.")
+                    lstNumber.Items.Add(strNumber)
+                Next
+
+            Else
+                MessageBox.Show("Please enter a positive number.")
+            End If
+
+        Else
+            MessageBox.Show("Please enter a number.")
+
+        End If
+    End Sub
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim intAverage As Integer
+        Dim intCount As Integer
+
+        intCount = 1
+        Do While lstNumber.Items.Count > intCount
+            intAverage += lstNumber.Items(intCount)
+            intCount += 1
+
+
+
+        Loop
+        MessageBox.Show("The average is " & intAverage / 5)
+    End Sub
+
+    Private Sub ctnClear_Click(sender As Object, e As EventArgs) Handles ctnClear.Click
+        lstNumber.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+End Class
